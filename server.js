71e1ca152dc645485b70e2e567f68266ed8385a6
pushpendra kumar <pushpendra466@gmail.com>
2020-04-18 10:01:19 +0530

    again from starting

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const signin=require('./controllers/signin');
 const profile=require('./controllers/profile');
 const image=require('./controllers/image');
 //const { response } = require('express');
-const db = knex({
+var db = require('knex')({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-    //   user : 'postgres',
-    //   password : 'tiger',
-    //   database : 'smart_brain'
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'tiger',
+      database : 'smart_brain'
     }
   });
 // db.select('*').from('users').then(data=>{
@@ -27,21 +26,21 @@ app.use(bodyParser.json());
 app.use(cors())
 
 app.get('/',(req,res)=>{
-    res.send('it is working');
+    res.send(database.users);
 })
 app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
 
 app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
 
 
-app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
+app.get('/profile/:id',(req,res)=>{msWriteProfilerMark.handleProfileGet(req,res,db)})
 
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 
 app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
-app.listen(process.env.PORT||'3000',()=>{
-    console.log(`app is running on port ${process.env.PORT}`);
+app.listen('3000',()=>{
+    console.log("app is running on port 3000");
 });
 
 
